26f71ffa8a413aab646542aad8623372b9b62d02
Ehsanul Nabil <nabilehsanul@gmail.com>
2026-07-23 23:49:54 +0600

    videos

--- routers/video.py
+++ routers/video.py
@@ -21,7 +21,7 @@ class VideoMetadataCreate(BaseModel):
     video_url: str
     public_id: str
 
-@router.post("/upload")
+@router.post("/videos")
 def create_video_metadata(
     payload: VideoMetadataCreate, 
     token: str, # Or extract from Authorization Header if using Bearer tokens
